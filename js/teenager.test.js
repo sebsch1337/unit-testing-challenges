17e223007d7ddad6274bf4f43ba76f8951ea43f1
Sebastian Scherbes <github@scherbes.de>
2022-09-14 15:53:40 +0200

    add test for check-teenager-status

--- js/teenager.test.js
+++ js/teenager.test.js
@@ -1,6 +1,15 @@
 import { checkTeenagerStatus } from "./teenager.js";
 
-test("returns correct teenager status", () => {
-  const teenagerStatus = checkTeenagerStatus(15);
-  expect(teenagerStatus).toBe(true);
+const ages = [
+  [12, false],
+  [13, true],
+  [15, true],
+  [19, true],
+  [20, false],
+  [201234, false],
+];
+
+test.each(ages)("returns correct teenager status", (age, isTeenager) => {
+  const teenagerStatus = checkTeenagerStatus(age);
+  expect(teenagerStatus).toBe(isTeenager);
 });
