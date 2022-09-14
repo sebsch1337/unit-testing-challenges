de3eaeca9f46743cc58739a622779c4ae6b781ab
Sebastian Scherbes <github@scherbes.de>
2022-09-14 16:24:46 +0200

    fix test to check for 0

--- js/calculations.test.js
+++ js/calculations.test.js
@@ -1,11 +1,11 @@
 import { cubicVolume } from "./calculations.js";
 
 const number = [
-  [0, 1, 2, true],
-  [-1, 2, 3, false],
-  [32, -34, 3, false],
-  [45, 43, -2, false],
-  [12, 34, 65, true],
+  [0, 1, 2, 0],
+  [-1, 2, 3, 0],
+  [32, -34, 3, 0],
+  [45, 43, -2, 0],
+  [2, 3, 4, 24],
 ];
 
 test.each(number)(
